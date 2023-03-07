69c2abad8ba564b429788b96e23d2b25d5035341
eva-kaloki <evatindi@gmail.com>
2023-03-07 09:54:40 -0800

    Task 9 updated

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# This program defines a function that takes a number and returns its last digit.
-
 def print_last_digit(number):
-    last_digit = number % 10  # using modulus to get the last digit
-    print(last_digit)  # printing the last digit
-   
-
+    if number < 0:
+        last_digit = number % -(10)
+        print(-(last_digit), end='')
+    else:
+        last_digit = number % 10
+        print(last_digit, end='')
+    return abs(last_digit)
