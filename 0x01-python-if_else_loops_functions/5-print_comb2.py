fcec3cc738e961748d7aa3b2c5d9cd471b5afef5
eva-kaloki <evatindi@gmail.com>
2023-03-07 09:41:43 -0800

    Task 5 updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# This program prints numbers from 0 to 99, separated by a comma and space, in ascending order
 for num in range(0, 99):
     print('{:02d}, '.format(num), end='')
 print('99')
