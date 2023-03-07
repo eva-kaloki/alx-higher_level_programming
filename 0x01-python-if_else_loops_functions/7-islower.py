e0e3bfe51dd175bc141ef7f00fc561decd83df84
eva-kaloki <evatindi@gmail.com>
2023-03-07 09:51:37 -0800

    Task 7 updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-
-# This program checks if a character is lowercase or not
 def islower(c):
-    " " "
-    Checks if a given character is lowercase .
-    
-    Arguments:
-    c -- the character to check
-
-    Returns:
-    True if c is lowercase, False otherwise
-    " " "
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
