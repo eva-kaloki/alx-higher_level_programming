6f1ef253cf63b91d9d1105fc4d0d5ef8c588b30e
eva-kaloki <evatindi@gmail.com>
2023-03-07 09:26:16 -0800

    Task 3 updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-# Prints the ASCII alphabet in lowercase without a new line
-for i in range(97, 123):
-    if chr(i) not in "eq":
-        print(f"{chr(i)}", end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
