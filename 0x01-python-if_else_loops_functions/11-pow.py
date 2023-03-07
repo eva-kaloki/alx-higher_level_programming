148244c0ba2fba076a756c1efbb97173d255d0d2
eva-kaloki <evatindi@gmail.com>
2023-03-07 09:59:34 -0800

    Task 11 updated

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-# Compute a to the power of b
 def pow(a, b):
-    # Initialize the result to 1
-    result = 1
-    # Multiply a by itself b times
-    for i in range(b):
-        result *= a
-    # Return the result
-    return result
-
+    return (a ** b)
