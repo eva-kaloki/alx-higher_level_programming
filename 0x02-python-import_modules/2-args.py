3dcd0cf7f907918315bafef5b6c3fae916160f23
eva-kaloki <evatindi@gmail.com>
2023-03-09 02:24:28 -0800

    task 2 updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number and list of arguments."""
-    import sys
+   """Print the number and list of arguments."""
+   import sys
 
     count = len(sys.argv) - 1
     if count == 0:
