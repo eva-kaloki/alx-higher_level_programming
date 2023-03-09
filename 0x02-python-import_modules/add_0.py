5473e74afce77b9026e192181a5f8ae77aeb5a3c
eva-kaloki <evatindi@gmail.com>
2023-03-09 01:54:05 -0800

    Task zero

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """An addition function
+
+    arguments:
+        a= first integer
+        b= second integer
+
+    Returns:
+        a + b
+
+    """
+
+    return (a + b)
