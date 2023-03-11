6ae5e7d7e8bf5cd53160b3584b52e5f644babcd0
eva-kaloki <evatindi@gmail.com>
2023-03-10 19:14:10 -0800

    Task 0 update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 # define a function named print_list_integer that takes a single parameter my_list
 def print_list_integer(my_list=[]):
-
     # iterate over each element in my_list
     for element in my_list:
         # print each integer element in the list using string formatting
         print("{:d}" .format(element))
-
-
-# create a list of integers
-my_list = [1, 2, 3, 4, 5]
-
-# call the print_list_integer and pass my_list as an argument
-print_list_integer(my_list)
