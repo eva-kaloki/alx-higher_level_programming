093bc2c98a0a49b0c5f6101a071bbd2d02a3b120
eva-kaloki <evatindi@gmail.com>
2023-03-15 06:04:15 -0700

    Task 3 updated

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+
+#a function that returns a set of all elements present in only one set (difference)
 def common_elements(set_1, set_2):
-    return (set_1 & set_2)
+    return (set_1.difference(set_2))
